02f2723c0e21a54ae5d1f5ff30b6fee7e567bc24
Pedro Peres <pedro@happyinspector.com>
2015-11-02 21:17:07 -0800

    Initial Commit

---
+++ AbbreviationLookup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AbbreviationLookup
+//
+//  Created by Pedro Peres on 11/2/15.
+//  Copyright (c) 2015 Pedro Peres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
